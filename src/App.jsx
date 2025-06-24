ce0d244c2a21e8bd63f1663ea54ba49ca6a4d91e
Paco-Humbert <humbert.paco@gmail.com>
2025-06-24 11:34:52 +0200

    Build : App.jsx, index.jsx : routes Build : SCSS -> maquette Add : pictures -> maquette

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,44 @@
-// src/App.jsx
+// On importe Routes et Route depuis react-router-dom pour gérer la navigation
 import { Routes, Route } from 'react-router-dom'
 
-// Pages
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Logement from './pages/Logements/Logements'
-import Error from './pages/Error/Error'
+// On importe les pages principales de l'application
+import Home from './pages/Home/Home.jsx'          // Page d’accueil
+import About from './pages/About/About.jsx'       // Page "À propos"
+import Logement from './pages/Logements/Logements.jsx' // Page de détail d’un logement
+import Error from './pages/Error/Error.jsx'       // Page d’erreur 404
 
-// Composants globaux
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+// On importe les composants affichés sur toutes les pages
+import Header from './components/Header/Header'  // En-tête du site
+import Footer from './components/Footer/Footer'  // Pied de page
 
+// Définition du composant principal App
 function App() {
   return (
     <>
+      {/* Le Header s’affiche sur toutes les pages */}
       <Header />
+
+      {/* Définition des routes de l’application */}
       <Routes>
-        {/* Page d’accueil */}
+
+        {/* Page d’accueil accessible à la racine ("/") */}
         <Route path="/" element={<Home />} />
 
-        {/* Page à propos */}
+        {/* Page "À propos", accessible à "/about" */}
         <Route path="/about" element={<About />} />
 
-        {/* Page logement dynamique (par ID) */}
+        {/* Page de détail d’un logement, dynamique selon l'ID (ex : /logements/abc123) */}
         <Route path="/logements/:id" element={<Logement />} />
 
-        {/* Page 404 si aucune autre route ne correspond */}
+        {/* Route de secours : affiche la page Error si l’URL ne correspond à aucune route */}
         <Route path="*" element={<Error />} />
       </Routes>
+
+      {/* Le Footer s’affiche sur toutes les pages */}
       <Footer />
     </>
   )
 }
 
+// On exporte App pour l’utiliser dans index.jsx
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // active React Router
 import App from './App'
 import './styles/index.scss' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Permet à toute l'application d'utiliser les routes */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
