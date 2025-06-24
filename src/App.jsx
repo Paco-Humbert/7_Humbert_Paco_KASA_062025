e51da28408f9e1901a97f17e931803b36d512dbc
Paco-Humbert <humbert.paco@gmail.com>
2025-06-24 10:56:18 +0200

    Build : App.jsx : routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Logement from './pages/Logements/Logements'
+import Error from './pages/Error/Error'
+
+// Composants globaux
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 function App() {
   return (
-    <div>
-      <h1>Bienvenue sur Kasa</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        {/* Page d’accueil */}
+        <Route path="/" element={<Home />} />
+
+        {/* Page à propos */}
+        <Route path="/about" element={<About />} />
+
+        {/* Page logement dynamique (par ID) */}
+        <Route path="/logements/:id" element={<Logement />} />
+
+        {/* Page 404 si aucune autre route ne correspond */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
