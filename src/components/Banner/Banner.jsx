fb21a056a221e0029b18c1389097c9b50512e013
Paco-Humbert <humbert.paco@gmail.com>
2025-06-24 12:03:13 +0200

    Build : Banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// importation image : pour accueil et "à propos"
+import BannerImgHome from '../../assets/images/ImageHome.png'
+import BannerImgAbout from '../../assets/images/ImageAbout.png'
+
+// importation du fichier de style SCSS spécifique à ce composant
+import './_Banner.scss'
