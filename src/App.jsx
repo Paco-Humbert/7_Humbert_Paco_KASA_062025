37671938d52afbceedd0fb9f23b72d09ead57e8e
Paco-Humbert <humbert.paco@gmail.com>
2025-07-01 10:19:37 +0200

    Refactor Build : SCSS Banner, Header

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,14 @@ import Footer from './components/Footer/Footer'
 function App() {
   return (
     <>
-      {/* Le Header s’affiche sur toutes les pages */}
       <Header />
-
       {/* Définition des routes de l’application avec options "future" activées */}
       <Routes future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        
-        {/* Page d’accueil accessible à la racine ("/") */}
-        <Route path="/" element={<Home />} />
-
-        {/* Page "À propos", accessible à "/about" */}
-        <Route path="/about" element={<About />} />
-
-        {/* Page de détail d’un logement, dynamique selon l’ID (ex : /logements/abc123) */}
-        <Route path="/logements/:id" element={<Logement />} />
-
-        {/* Route de secours : affiche la page Error si l’URL ne correspond à aucune route */}
-        <Route path="*" element={<Error />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/logements/:id" element={<Logement />} />
+      <Route path="*" element={<Error />} />
       </Routes>
-
-      {/* Le Footer s’affiche sur toutes les pages */}
       <Footer />
     </>
   )
