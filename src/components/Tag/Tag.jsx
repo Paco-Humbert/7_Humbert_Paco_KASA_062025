2fb3af1af83b3971cb95388427962a06e197de40
Paco-Humbert <humbert.paco@gmail.com>
2025-07-06 15:39:06 +0200

    Build : tag.jsx

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import DataLogements from '../../datas/logements.json'
+import './_Tag.scss'
+
+// Création d'une fonction composant Tag
+function Tag() {
+    // Récupération de l'ID de la location avec le hook useParams de React Router
+    const { id } = useParams()
+    const location = DataLogements.find((location) => location.id === id)
+
+    return (
+
+        <div className='tags'>
+            <ul className='tags__container'>
+                {location.tags.map((tag, index) => (
+                    <li key={index} className='tags__container-item'>
+                        {tag}
+                    </li>
+                ))}
+            </ul>
+        </div>
+        
+    )
+}
+
+export default Tag
