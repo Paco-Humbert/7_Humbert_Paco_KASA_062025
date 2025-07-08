b5633ee3d699e549497a716925b99f013686d6c7
Paco-Humbert <humbert.paco@gmail.com>
2025-07-08 14:29:42 +0200

    refactor display

--- src/components/Cards/cards.jsx
+++ src/components/Cards/cards.jsx
@@ -5,16 +5,19 @@ import './_Cards.scss'
 
 // Création d'une fonction composant LogementsList récupérant les données de chaque location du JSON
 function LogementsList() {
-
+const isHome = location.pathname === '/';
     return (
 
         <section className='logements'>
             <div className='logements__list'>
                 {DataLogements.map((location) => (
                     <div key={location.id} className='logements__list-item'>
-                        <Link to={"/logements/" + location.id} className='logements__list-item-card'>
-                            <img src={location.cover} alt={location.title} className=' logements-cover'/>
-                            <h2 className='logements-title'>{location.title}</h2>
+                        <Link to={'/logements/' + location.id} className="logements__list-item-card">
+                        <div className="image-wrapper">
+                            <img src={location.cover} alt={location.title} className="logements-cover" />
+                            {isHome && <div className="overlay"></div>}
+                        </div>
+                        <h2 className="logements-title">{location.title}</h2>
                         </Link>
                     </div>
                 ))}
